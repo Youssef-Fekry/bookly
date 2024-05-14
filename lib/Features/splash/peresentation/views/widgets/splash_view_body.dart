6e9e55780daa734da1d452d3a10dae8922ec43b2
Youssef Fekry <yousseffekry14@gmail.com>
2024-05-14 17:00:46 +0300

    splash screen Done

--- lib/Features/splash/peresentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/peresentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,42 @@
 import 'package:bokly_app/core/utils/assetsdata.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lottie/lottie.dart';
 
-class SplashViewBody extends StatelessWidget {
+import 'sliding_text.dart';
+
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+
+    void dispose() {
+      super.dispose();
+      animationController.dispose();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +44,15 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.MainLogo),
+        Flexible(child: Image.asset(AssetsData.MainLogo)),
+        Flexible(
+          child: Lottie.network(
+            'https://lottie.host/2875bcb5-b647-41e2-a547-d725ef644d90/zk5kLwomf4.json',
+          ),
+        ),
+        Flexible(
+          child: SlidingText(slidingAnimation: slidingAnimation),
+        ),
       ],
     );
   }
