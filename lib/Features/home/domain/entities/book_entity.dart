b3c4861ddf3524f9426b415aedaa0751389cdd93
Youssef Fekry <yousseffekry14@gmail.com>
2024-05-28 03:32:50 +0300

    book entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity {
+  final String img;
+  final String authorname;
+  final String title;
+  final num price;
+  final num rating;
+
+  BookEntity({required this.img, required this.authorname, required this.title, required this.price, required this.rating});
+}
