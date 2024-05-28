b8ac75ded1a9ba6de40d209415431f7241eb8dd9
Youssef Fekry <yousseffekry14@gmail.com>
2024-05-28 03:32:35 +0300

    search view done

--- lib/Features/search/presentation/widgets/search_view_body.dart
+++ lib/Features/search/presentation/widgets/search_view_body.dart
@@ -1,6 +1,6 @@
-import 'package:bokly_app/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bokly_app/Features/search/presentation/widgets/customsearch.dart';
 import 'package:bokly_app/Features/search/presentation/widgets/resultsearch.dart';
+import 'package:bokly_app/core/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,8 +12,19 @@ class SearchViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomeSearchTextFiled(),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Best Seller',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 16,
+          ),
           Expanded(child: ResultSearchView()),
         ],
       ),
