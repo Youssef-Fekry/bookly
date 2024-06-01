06933ca0a899fed5394b7e7c76b1cb2a50776f2d
Youssef Fekry <yousseffekry14@gmail.com>
2024-06-02 00:31:01 +0300

    creating data layer

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,9 +1,16 @@
 class BookEntity {
-  final String img;
-  final String authorname;
-  final String title;
-  final num price;
-  final num rating;
+  final String? bookId;
+  final String? img;
+  final String? authorname;
+  final String? title;
+  final num? price;
+  final num? rating;
 
-  BookEntity({required this.img, required this.authorname, required this.title, required this.price, required this.rating});
+  BookEntity(
+      {required this.bookId,
+      required this.img,
+      required this.authorname,
+      required this.title,
+      required this.price,
+      required this.rating});
 }
--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -3,10 +3,7 @@ import 'package:bokly_app/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
 
-  
-  Future<Either< Failure ,List<BookEntity>>> fetchFeaturedBooks();
-
-
-  Future<Either< Failure ,List<BookEntity>>> fetchNewstBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewstBooks();
 }
